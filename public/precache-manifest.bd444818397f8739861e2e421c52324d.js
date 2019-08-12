c90649290fe82304941067dde4552c745db17b2f
Gulfiya Isyangulova <isyangulova2013@gmail.com>
2019-08-12 19:48:18 -0400

    deploying

---
+++ public/precache-manifest.bd444818397f8739861e2e421c52324d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc2e0c5ac59008a1cf5fd81496957622",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35c59729444593c9a66b",
+    "url": "/static/css/main.d668cb7f.chunk.css"
+  },
+  {
+    "revision": "d88f40b1e751444c72b6",
+    "url": "/static/js/2.88d2bee7.chunk.js"
+  },
+  {
+    "revision": "35c59729444593c9a66b",
+    "url": "/static/js/main.19f1210c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
