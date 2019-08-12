009874be44302cea4437a576e1ffc7e7d6125bec
Gulfiya Isyangulova <isyangulova2013@gmail.com>
2019-08-12 19:57:17 -0400

    dfs

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ require('./config/passport-stuff');
 
 
 mongoose
-  .connect('mongodb://localhost/jiu-jitsu-locator-project', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
